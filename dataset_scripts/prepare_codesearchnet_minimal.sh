6768721af099689fe1e06099ff6fc7f86cc79ea5
Leonhard Applis <Leonhard.Applis@Protonmail.com>
2022-09-07 15:55:54 +0200

    reduced minimal of minimal datasets to 50

--- dataset_scripts/prepare_codesearchnet_minimal.sh
+++ dataset_scripts/prepare_codesearchnet_minimal.sh
@@ -8,8 +8,8 @@ cd ../datasets/codesearchnet/raw
 # Trim the dataset
 echo "Trimming the dataset..."
 
-echo "$(head -n 1000 test/test.jsonl)" > test/test.jsonl
-echo "$(head -n 1000 train/train.jsonl)" > train/train.jsonl
-echo "$(head -n 1000 valid/valid.jsonl)" > valid/valid.jsonl
+echo "$(head -n 50 test/test.jsonl)" > test/test.jsonl
+echo "$(head -n 50 train/train.jsonl)" > train/train.jsonl
+echo "$(head -n 50 valid/valid.jsonl)" > valid/valid.jsonl
 
 echo "Dataset prepared."
--- dataset_scripts/prepare_funcom_minimal.sh
+++ dataset_scripts/prepare_funcom_minimal.sh
@@ -8,8 +8,8 @@ cd ../datasets/funcom/raw
 # Trim the dataset
 echo "Trimming the dataset..."
 
-echo "$(head -n 1000 test/functions.test.jsonl)" > test/functions.test.jsonl
-echo "$(head -n 1000 train/functions.train.jsonl)" > train/functions.train.jsonl
-echo "$(head -n 1000 valid/functions.val.jsonl)" > valid/functions.val.jsonl
+echo "$(head -n 50 test/functions.test.jsonl)" > test/functions.test.jsonl
+echo "$(head -n 50 train/functions.train.jsonl)" > train/functions.train.jsonl
+echo "$(head -n 50 valid/functions.val.jsonl)" > valid/functions.val.jsonl
 
 echo "Dataset prepared."
