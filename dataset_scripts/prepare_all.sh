e6bd072fa5cff36fa56e3ef95ad07e3cbde4d032
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-09-19 12:47:05 +0300

    Add scripts for all dataset preparation

---
+++ dataset_scripts/prepare_all.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash prepare_codesearchnet.sh
+bash prepare_default.sh
+bash prepare_funcom.sh
---
+++ dataset_scripts/prepare_all_minimal.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bash prepare_codesearchnet_minimal.sh
+bash prepare_default_minimal.sh
+bash prepare_funcom_minimal.sh
