8f3c51fce90d8d6da2eab5fba4d80b62f31fe024
bmorkunas <balys.morkunas@gmail.com>
2022-10-09 11:10:03 +0300

    calculate dataset statistics

---
+++ dataset_scripts/dataset_statistics.py
@@ -0,0 +1,33 @@
+import json
+import os
+from pathlib import Path
+
+DATASETS = ["codesearchnet", "default", "funcom"]
+
+for ds in DATASETS:
+
+    path = Path("../datasets/" + ds + "/raw/")
+    file_paths = [os.path.join(dirpath,f) for (dirpath, dirnames, filenames) in os.walk(path) for f in filenames]
+
+    examples = 0
+    inline_comment_count = 0
+
+    # go over each file and collect its json
+    for path in file_paths:
+        with open(path, "r") as file:
+            lines = file.readlines()
+
+            for line in lines:
+                examples += 1
+                data = json.loads(line)
+                code = str(data["original_string"])
+                comment = str(data["docstring"])
+
+                # Count inline comments
+                if "//" in code or "/*" in code:
+                    inline_comment_count += 1
+
+    print("=========" + ds + "=========")
+    print("TOTAL NUMBER OF EXAMPLES       : " + str(examples))
+    print("TOTAL NUMBER OF INLINE COMMENTS: " + str(inline_comment_count))
+    print("========================================")
