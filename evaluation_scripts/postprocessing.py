0ab62e8bc9f60a34697a78c80828b0eb8a1e41cb
Vidas Bacevicius <V.Bacevicius@tudelft.nl>
2022-10-10 08:10:28 +0300

    Postprocessing init cleanup

--- evaluation_scripts/postprocessing.py
+++ evaluation_scripts/postprocessing.py
@@ -1,51 +1,46 @@
+from argparse import ArgumentParser
+import nltk
 from nltk.tokenize import word_tokenize
 from nltk.translate.bleu_score import sentence_bleu
 from nltk.translate.bleu_score import SmoothingFunction
 import scipy.stats as stats
-from bisect import bisect_left
-from typing import List
 from cliffs_delta import cliffs_delta
 
-no_com_jac = []
-com_jac = []
-no_com_bleu = []
-com_bleu = []
+nltk.download("punkt")
 
-references = open("ref.txt").readlines()
-predictions_com = open("pred_com.txt").readlines()
-predictions_no_com = open("pred_no_com.txt").readlines()
+# Get the name of the trained model from args
+parser = ArgumentParser()
+parser.add_argument(
+    "-d",
+    "--dataset",
+    dest="model_name",
+    required=True,
+)
+
+args = parser.parse_args()
+model_name = args.model_name
+
+references = open(f"../models/trained_{model_name}/ref.txt").readlines()
+predictions  = open(f"../models/trained_{model_name}/pred.txt").readlines()
+
+jaccard_distance = []
+bleu_score = []
 
 for i, ref in enumerate(references):
     ref_tokens = set(word_tokenize(ref))
-    com_pred_tokens = set(word_tokenize(predictions_com[i]))
-    no_com_pred_tokens = set(word_tokenize(predictions_no_com[i]))
+    pred_tokens = set(word_tokenize(predictions[i]))
 
     # Jaccard distance
-    jac = 1 - (len(ref_tokens & com_pred_tokens) / len(ref_tokens | com_pred_tokens))
-    jac_no = 1 - (
-        len(ref_tokens & no_com_pred_tokens) / len(ref_tokens | no_com_pred_tokens)
-    )
-    com_jac.append(jac)
-    no_com_jac.append(jac_no)
+    jac = 1 - (len(ref_tokens & pred_tokens) / len(ref_tokens | pred_tokens))
+    jaccard_distance.append(jac)
 
     # BLEU score
     bleu = sentence_bleu(
-        ref_tokens, com_pred_tokens, smoothing_function=SmoothingFunction().method1
+        ref_tokens, pred_tokens, smoothing_function=SmoothingFunction().method1
     )
-    bleu_no = sentence_bleu(
-        ref_tokens, no_com_pred_tokens, smoothing_function=SmoothingFunction().method1
-    )
-    com_bleu.append(bleu)
-    no_com_bleu.append(bleu_no)
+    bleu_score.append(bleu)
 
-print(
-    "Rank Sum with BLEU:\n", stats.ranksums(com_bleu, no_com_bleu, alternative="less")
-)
-print(
-    "Rank Sum  with Jaccard Distance:\n",
-    stats.ranksums(com_jac, no_com_jac, alternative="less"),
-)
-
-
-print("BLEU Cliff Delta:", cliffs_delta(com_bleu, no_com_bleu))
-print("Jaccard Cliff Delta:", cliffs_delta(com_jac, no_com_jac))
+print("Jaccard distances: " )
+print(jaccard_distance)
+print("BLEU scores: ")
+print(bleu_score)
